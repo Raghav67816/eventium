4bdb6ab5bc577bda325f8a2df07c73edae00c05d
Raghav67816 <kumaraghav079@gmail.com>
2025-11-06 01:02:41 +0530

    added login screen, going to sleep now

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,25 +1,17 @@
-import { Stack, Link } from 'expo-router';
-
-import { View } from 'react-native';
-
 import { Button } from '@/components/Button';
-import { Container } from '@/components/Container';
-import { ScreenContent } from '@/components/ScreenContent';
+import { View, Text, TextInput } from 'react-native';
 
 export default function Home() {
   return (
     <View className={styles.container}>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
+      <Text className={'text-3xl font-semibold mt-20'}>Log In</Text>
+      <TextInput className={'border-b mt-8'} placeholder='Your Email' textContentType={'emailAddress'} />
+      <TextInput className={'border-b mt-8'} placeholder='Your Password' textContentType={'password'} />
+      <Button title={'Submit'} className='mt-20 bg-black'></Button>
     </View>
   );
 }
 
 const styles = {
-  container: 'flex flex-1 bg-white',
+  container: 'p-8',
 };
