54a58ded1610daaedc0bbff8b4a9487c28b5b4af
Raghav67816 <kumaraghav079@gmail.com>
2025-11-06 00:47:13 +0530

    fixed inital config

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,9 +1,7 @@
-import { Stack } from "expo-router";
+import '../global.css';
 
-export default function RootLayout() {
-  return <Stack
-    screenOptions={{
-      headerShown: false
-    }}
-  />;
+import { Stack } from 'expo-router';
+
+export default function Layout() {
+  return <Stack />;
 }
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,8 +1,25 @@
-import '../globals.css'
-import { Text, View } from "react-native";
- 
-export default function App() {
+import { Stack, Link } from 'expo-router';
+
+import { View } from 'react-native';
+
+import { Button } from '@/components/Button';
+import { Container } from '@/components/Container';
+import { ScreenContent } from '@/components/ScreenContent';
+
+export default function Home() {
   return (
-    <View></View>
+    <View className={styles.container}>
+      <Stack.Screen options={{ title: 'Home' }} />
+      <Container>
+        <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
+        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
+          <Button title="Show Details" />
+        </Link>
+      </Container>
+    </View>
   );
 }
+
+const styles = {
+  container: 'flex flex-1 bg-white',
+};
