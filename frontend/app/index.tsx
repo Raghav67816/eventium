b15779bc465a43412a762a9574854bc1d0f035c7
Raghav67816 <kumaraghav079@gmail.com>
2025-11-06 00:00:52 +0530

    basic react native setup

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,8 @@
+import '../globals.css'
 import { Text, View } from "react-native";
-
-export default function Index() {
+ 
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello, World</Text>
-    </View>
+    <View></View>
   );
 }
