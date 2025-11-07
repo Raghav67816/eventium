de56dc2d867c187acef8534d2e6d777c77159110
Raghav67816 <kumaraghav079@gmail.com>
2025-11-07 23:37:03 +0530

    minor improvements in OrgCard layout

--- frontend/app/event-page.tsx
+++ frontend/app/event-page.tsx
@@ -7,9 +7,9 @@ import OrgCard from "@/components/OrgCard";
 export default function EventPage() {
     const { eventName, eventId } = useLocalSearchParams();
     const [isVisible, setVisible] = useState(false);
-    
 
-    function toggleModal(){
+
+    function toggleModal() {
         setVisible(!isVisible);
     }
 
@@ -19,34 +19,40 @@ export default function EventPage() {
                 visible={isVisible}
                 animationType="slide"
                 transparent={true}
-                >
+            >
                 <View className="flex-1 justify-center items-center bg-black/50">
                     <View className="w-5/6 h-1/2 bg-white rounded-xl p-6 shadow-2xl">
-                    <View className={"flex-row items-center justify-between"}>
-                        <Text className="text-xl font-bold">Organisers</Text>
-                        <IconButton iconName="close-outline" iconSize={18} onPressExec={toggleModal} ></IconButton>
-                    </View>
-                    <View className={"flex-row gap-4"}>
-                        <ScrollView className={"mt-8 mb-8 flex-shrink-0"}>
-                            <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
-                            <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
-                            <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
-                            <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
-                            <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
-                            <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
-                            <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
-                            <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
-                            <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
-                            <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
-                            <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
-                            <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
-                            <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
-                            <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
-                        </ScrollView>
-                    </View>
+                        <View className={"flex-row items-center justify-between"}>
+                            <Text className="text-xl font-bold">Organisers</Text>
+                            <IconButton iconName="close-outline" iconSize={18} onPressExec={toggleModal} ></IconButton>
+                        </View>
+                        <View className={"mb-32"}>
+                            <View className={"mb-8"}>
+                                <ScrollView className={"mt-8 mb-8"}>
+                                    <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
+                                    <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
+                                    <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
+                                    <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
+                                    <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
+                                    <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
+                                    <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
+                                    <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
+                                    <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
+                                    <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
+                                    <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
+                                    <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
+                                    <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
+                                    <OrgCard name={"Raghav Kumar"} email={"kumaraghav079@gmail.com"} ></OrgCard>
+                                </ScrollView>
+                                <View className={"flex-row flex justify-between"}>
+                                    <TextInput style={{minWidth: '450'}} className={"border"} placeholder="Email"></TextInput>
+                                    <Button title="Hello"></Button>
+                                </View>
+                            </View>
+                        </View>
                     </View>
                 </View>
-                </Modal>
+            </Modal>
             <View className={"flex-row justify-between py-4 border-b"}>
                 <View className={"flex-row gap-4 items-center"}>
                     <IconButton iconName={"arrow-back"} iconSize={18} onPressExec={toggleModal} />
